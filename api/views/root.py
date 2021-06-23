1dfb6d8fc89f78d012478980b78e52f60e53ad4d
Stephen Oba <obastepheno@gmail.com>
2021-06-23 14:29:02 +0100

    updated available endpoints

--- api/views/root.py
+++ api/views/root.py
@@ -8,8 +8,9 @@ from rest_framework.reverse import reverse
 def AVAILABLE_APIS(request, format):  # noqa
     return [
         ("twitter_login", reverse("twitter-login", request=request, format=format)),
+        ("rest_logout", reverse("rest_logout", request=request, format=format)),
         ("tips", reverse("tip-list", request=request, format=format)),
-        ("entry-list", reverse("entry-list", request=request, format=format)),
+        ("entry_list", reverse("entry-list", request=request, format=format)),
     ]
 
 
@@ -20,4 +21,5 @@ def api_root(request, format=None):
     Display all available urls.
     """
     apis = AVAILABLE_APIS(request, format)
+
     return Response(collections.OrderedDict(apis))
